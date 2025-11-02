8c203c62d071c3465d7ed5e67c979dd1197b92e1
EX154446@edp.pt <j141996@hotmail.com>
2025-11-02 02:51:13 +0000

    pok

---
+++ src/RedditClone.API/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using Shared.User.Contracts;
+
+namespace RedditClone.API.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<Guid?> GetCurrentUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
